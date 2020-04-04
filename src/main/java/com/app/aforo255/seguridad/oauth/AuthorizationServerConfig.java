884f29432e79b1b67091ae5d7d70e6872465362a
Jorge Galeas <jodagab4@hotmail.com>
2020-04-04 14:12:22 -0500

    upgrate

---
+++ src/main/java/com/app/aforo255/seguridad/oauth/AuthorizationServerConfig.java
@@ -0,0 +1,65 @@
+package com.app.aforo255.seguridad.oauth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig  extends AuthorizationServerConfigurerAdapter{
+
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	@Override
+	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+		security.checkTokenAccess("permitAll()").checkTokenAccess("isAuthenticated()");
+	}
+
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		// TODO Auto-generated method stub
+		//Cliente angular se matricula para que ingrese
+		//variable en el server config
+		// secret 123456
+		clients.inMemory().withClient("webAngular")
+		.secret(passwordEncoder.encode("123456"))
+		.scopes("read","write")
+		.authorizedGrantTypes("password", "refresh_token")
+		.accessTokenValiditySeconds(3600)
+		.refreshTokenValiditySeconds(3600);
+		
+		
+	}
+	
+	
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.authenticationManager(authenticationManager)
+		.tokenStore(tokenSore()).accessTokenConverter(accessTokenConverter());
+	}
+
+	@Bean
+	public JwtTokenStore tokenSore() {
+		return new JwtTokenStore(accessTokenConverter());	
+	}
+	
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+		jwtAccessTokenConverter.setSigningKey("aforo255");
+		return jwtAccessTokenConverter;
+	}
+	
+}
